57b6a08754841e66c309091f231df418631c419d
Fluturecode <fluturecode@gmail.com>
2023-07-28 19:19:48 +0200

    added NRCReader component

---
+++ src/components/NFCReader.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import NFC from 'react-nfc';
+
+export const NFCReader = ({ onTagDetected }) => {
+  const [isNFCSupported, setNFCSupported] = useState(true);
+
+  useEffect(() => {
+    if (!NFC.isSupported()) {
+      setNFCSupported(false);
+    }
+  }, []);
+
+  const handleNfcDetection = (tag) => {
+    // Pass the detected NFC tag to the parent component.
+    onTagDetected(tag);
+  }
+
+  return (
+    <div>
+    {isNFCSupported ? (
+      <NFC onTagDetected={handleNfcDetection}>
+        {(status) => <p>Status: {status}</p>}
+      </NFC>
+    ) : (
+      <p>NFC is not supported on this device.</p>
+    )}
+  </div>
+  )
+}
